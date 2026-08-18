2a4dc94d20d7a63d51986b2bc07ee4cae76836b9
Thibault Saldou <thibaultsaldou@yahoo.com>
2026-08-18 12:16:32 +0200

    added models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :members, dependent: :destroy
+  has_many :households, through: :members
+  has_many :created_households, class_name: "Household", foreign_key: "user_id", dependent: :destroy
+  has_many :chats, dependent: :nullify
 end
