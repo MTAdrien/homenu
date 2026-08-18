a379943d3e5c73557a51fa85e81baf094d9961e5
Thibault Saldou <thibaultsaldou@yahoo.com>
2026-08-18 12:46:53 +0200

    added some actions

--- app/controllers/fridge_items_controller.rb
+++ app/controllers/fridge_items_controller.rb
@@ -1,16 +1,43 @@
 class FridgeItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_fridge_item, only: [:edit, :update, :destroy]
+
   def new
+    @fridge_item = FridgeItem.new
   end
 
   def create
+    @fridge_item = FridgeItem.new(fridge_item_params)
+    if @fridge_item.save
+      redirect_to fridge_items_path, notice: "Article ajouté."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @fridge_item.update(fridge_item_params)
+      redirect_to fridge_items_path, notice: "Article mis à jour."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @fridge_item.destroy
+    redirect_to fridge_items_path, notice: "Article supprimé.", status: :see_other
+  end
+
+  private
+
+  def set_fridge_item
+    @fridge_item = FridgeItem.find(params[:id])
+  end
+
+  def fridge_item_params
+    params.require(:fridge_item).permit(:name, :quantity, :expiration_date)
   end
 end
