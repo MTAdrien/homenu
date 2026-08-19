f1cf9ac3dae8db94fa2206f25e0984911db9722d
Thibault Saldou <thibaultsaldou@yahoo.com>
2026-08-19 16:15:37 +0200

    changed buttons

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,7 @@ class Member < ApplicationRecord
   belongs_to :household
   belongs_to :user
 
-  ROLES = %w[admin membre].freeze
+  ROLES = %w[admin member].freeze
 
   validates :first_name, presence: true
   validates :role, presence: true, inclusion: { in: ROLES }
