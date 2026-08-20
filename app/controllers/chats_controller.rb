629d3d0475f83b46e9ccac3411f2c5080111d377
Tayayi Muyard <muyardmayam.t@gmail.com>
2026-08-20 12:29:22 +0200

    feat: add create action

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,19 @@
 class ChatsController < ApplicationController
   before_action :authenticate_user!
 
+  def create
+    @household = Household.find(params[:household_id])
+    @chat = Chat.new(title: "Untitled")
+    @chat.household = @household
+    @chat.user = current_user
+
+    if @chat.save
+      redirect_to chat_path(@chat)
+    else
+      @chats = @household.chats.where(user: current_user)
+      render "household/show"
+    end
+  end
   def show
     @chat    = current_user.chats.find(params[:id])
     @message = Message.new
