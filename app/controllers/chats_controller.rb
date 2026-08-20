0c2ee58e003be5e5190c0d7bdc54335975faa29d
Tayayi Muyard <muyardmayam.t@gmail.com>
2026-08-20 13:54:13 +0200

    fix: corrects the logic of chats controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,25 +2,23 @@ class ChatsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @chats = Chat.all
+    @chats = current_user.household.chats
   end
 
   def show
-    @chat    = current_user.chats.find(params[:id])
+    @chat    = current_user.household.chats.find(params[:id])
     @message = Message.new
   end
 
   def create
-    @household = Household.find(params[:household_id])
-    @chat = Chat.new(title: "Untitled")
-    @chat.household = @household
-    @chat.user = current_user
+    @household = current_user.household
+    @chat = @household.chats.build(title: "Untitled", user: current_user)
 
     if @chat.save
       redirect_to chat_path(@chat)
     else
-      @chats = @household.chats.where(user: current_user)
-      render "household/show"
+      @chats = @household.chats
+      render "households/show"
     end
   end
 end
